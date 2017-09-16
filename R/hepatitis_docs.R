5d685bcc8d18ec24802a8c699a14a029b7d14123
James J Balamuta <balamut2@illinois.edu>
2017-09-16 14:53:25 -0500

    Add hepatitis data set

---
+++ R/hepatitis_docs.R
@@ -0,0 +1,58 @@
+#' Hepatitis Data Set
+#'
+#' This data set contains information on folks that suffer from hepatitis.
+#'
+#' @format A data frame with 6497 observations (1599 Red and 4898 White) on the following 12 variables.
+#' - `class`
+#'     - Die or Live
+#' - `age`
+#'     - Integer
+#' - `sex`
+#'     - Male, Female
+#' - `steroid`
+#'     - No, Yes
+#' - `antivirals`
+#'     - No, Yes
+#' - `fatigue`
+#'     - No, Yes
+#' - `malaise`
+#'     - No, Yes
+#' - `anorexia`
+#'     - No, Yes
+#' - `liver_big`
+#'     - No, Yes
+#' - `liver_firm`
+#'     - No, Yes
+#' - `spleen_palpable`
+#'     - No, Yes
+#' - `spiders`
+#'     - No, Yes
+#' - `ascites`
+#'     - No, Yes
+#' - `varices`
+#'     - No, Yes
+#' - `bilirubin`
+#'     - Numeric
+#'     - This can also be treated as a factor
+#' - `alk_phosphate`
+#'     - Integer
+#' - `sgot`
+#'     - Integer
+#' - `albumin`
+#'     - Numeric
+#' - `protime`
+#'     - Integer
+#' - `histology`
+#'     - No, Yes
+#' @source
+#' G.Gong (Carnegie-Mellon University) via
+#' Bojan Cestnik
+#' Jozef Stefan Institute
+#' Jamova 39
+#' 61000 Ljubljana
+#' Yugoslavia (tel.: (38)(+61) 214-399 ext.287)
+#'
+#' @references
+#' <https://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data>
+#' <https://archive.ics.uci.edu/ml/datasets/hepatitis>
+"hepatitis"
---
+++ data-raw/hepatitis_build.R
@@ -0,0 +1,52 @@
+## UCI Data
+# Hepatitis Data http://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data
+
+url_hepatitis = "http://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data"
+
+hepatitis = read.csv(url_hepatitis,
+                   header = FALSE, na.strings = "?")
+
+# Columns taken verbatim from ML page
+# Regex search with: [0-9]{1,2}\. (.*):.*
+# Replacement: "\1",
+var_names = c(
+  "Class",
+  "AGE",
+  "SEX",
+  "STEROID",
+  "ANTIVIRALS",
+  "FATIGUE",
+  "MALAISE",
+  "ANOREXIA",
+  "LIVER BIG",
+  "LIVER FIRM",
+  "SPLEEN PALPABLE",
+  "SPIDERS",
+  "ASCITES",
+  "VARICES",
+  "BILIRUBIN",
+  "ALK PHOSPHATE",
+  "SGOT",
+  "ALBUMIN",
+  "PROTIME",
+  "HISTOLOGY"
+)
+
+var_names_safe = gsub("[[:space:]]", "_", var_names)
+
+# Label columns
+colnames(hepatitis) = tolower(var_names_safe)
+
+# Make into a dichotomous variable marked by a factor
+hepatitis[, c(4:14, 20)] = lapply(hepatitis[, c(4:14, 20)], factor, labels = c("No", "Yes"))
+
+# Switch to being factor based
+hepatitis = within(hepatitis,{
+  class = factor(class, labels = c("Die", "Live"))
+  sex   = factor(sex, labels = c("Male", "Female"))
+})
+
+devtools::use_data(hepatitis, overwrite = TRUE)
+
+## output colnames
+cat(paste0(colnames(hepatitis),"\n"), sep="")
