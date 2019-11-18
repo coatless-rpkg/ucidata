1110b8efd383b378a9b725b0fb0b66617d529cac
daviddalpiaz <dalpiaz2@illinois.edu>
2019-11-17 20:39:50 -0600

    point documentation to correct wine dataset

--- R/wine_docs.R
+++ R/wine_docs.R
@@ -28,6 +28,6 @@
 #' Modeling wine preferences by data mining from physicochemical properties.
 #' In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236.
 #' @references
-#' <https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.names>
-#' <https://archive.ics.uci.edu/ml/datasets/wine>
+#' <https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality.names>
+#' <https://archive.ics.uci.edu/ml/datasets/Wine+Quality>
 "wine"
